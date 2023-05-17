241eb99edb3f7a12611824b169512643c85efee9
cookiekingjlim <133620407+cookiekingjlim@users.noreply.github.com>
2023-05-17 15:14:21 +0900

    00_Practice

---
+++ 00_Practice/src/com/kh/practice/OperatorPractice.java
@@ -0,0 +1,93 @@
+package com.kh.practice;
+
+import java.util.Scanner;
+
+public class OperatorPractice {
+
+	public static void main(String[] args) {
+		OperatorPractice op = new OperatorPractice();
+//		op.method1();
+//		op.method2();
+//		op.method3();
+//		op.method4();
+//		op.method5();
+		op.method6();
+	}
+	
+	public void method1() {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("인원수 : ");
+		int num1 = sc.nextInt();
+		System.out.print("연필 개수 : ");
+		int num2 = sc.nextInt();
+		System.out.println("1인당 연필 개수 :" + (num2 / num1));
+		System.out.println("남는 연필 개수 :" + (num2 % num1));
+		
+
+	}
+
+	public void method2() {
+		Scanner sc = new Scanner(System.in);		
+		
+		System.out.print("양수를 입력해주세요 > ");
+		int num = sc.nextInt();
+		
+		System.out.println(num/100*100);
+		
+	}
+	
+	public void method3() {
+		Scanner sc = new Scanner(System.in);
+		
+		
+		System.out.print("입력1 :");
+		int num1 = sc.nextInt();
+		
+		System.out.print("입력2 :");
+		int num2 = sc.nextInt();
+		
+		System.out.print("입력3 :");
+		int num3 = sc.nextInt();
+		
+		System.out.println((num1 == num2) && (num2 == num3));
+	
+		
+	}
+	
+	public void method4() {
+		Scanner sc = new Scanner(System.in);
+		int num = 0;
+		
+		System.out.print("정수 : ");
+		num = sc.nextInt();
+		
+		System.out.println((num % 2 == 0) ? "짝수이다." : "홀수이다.");
+		
+	}
+	
+	public void method5() {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("나이 : ");
+		int num = sc.nextInt();
+		
+		System.out.println ((num <= 13) ? "어린이다." : (num > 13) && (num <= 19) ? "청소년이다." : "어른이다.");
+	
+	}
+	
+	public void method6() {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("사과의 개수 : ");
+		int num1 = sc.nextInt();
+		
+		System.out.print("바구니의 개수 : ");
+		int num2 = sc.nextInt();
+		
+		int num3 = num1/num2;
+		System.out.print("필요한 바구니의 개수 : " + ++num3);
+	}
+	
+}
+
---
+++ 00_Practice/src/com/kh/practice/VariablePractice.java
@@ -0,0 +1,93 @@
+package com.kh.practice;
+
+import java.util.Scanner;
+
+public class VariablePractice {
+	
+	// FQCN(Fully Qualified Class Name)
+	// 클래스가 속한 패키지명을 모두 포함한 이름
+
+	public static void main(String[] args) {
+		
+		VariablePractice vp = new VariablePractice();
+//		vp.method1();
+//		vp.method2();
+//		vp.method3();
+		vp.method4();
+	}
+	
+	public void method1() {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("첫 번째 정수를 입력하세요 : ");
+		int num1 = sc.nextInt();
+		
+		System.out.print("두 번째 정수를 입력하세요 : ");
+		int num2 = sc.nextInt();
+		
+		System.out.println("더하기 결과 : " +(num1+num2));
+		System.out.println("빼기 결과 : " +(num1-num2));
+		System.out.println("곱하기 결과 : " + (num1*num2));
+		System.out.println("나누기 몫 결과 : " +(num1/num2));
+		
+		
+	}
+	
+	
+	public void method2() {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("영화관의 요금표는 다음과 같습니다.");
+		
+		System.out.print("성인 요금 : "); //여기에 ~원을 넣고 싶은데 어떻게 넣지...?
+		int num1 = sc.nextInt();
+		
+		System.out.print("청소년 요금 : ");
+		int num2 = sc.nextInt();
+		
+		System.out.println("성인 2명 : " + (num1*2) +"원");
+		System.out.println("청소년 3명 : " + (num2*3) +"원");
+		
+		System.out.println("총 금액 : " + ((num1*2) + (num2*3)) + "원");
+	}
+	
+
+	public void method3() {
+		int x = 5;
+		int y = 7;
+		int z = 9;
+		
+		final int X;
+		x = 7;
+		final int Y;
+		y = 9;
+		final int Z;
+		z = 5;
+		
+		
+		System.out.println("x=" + x);
+		System.out.println("y=" + y);
+		System.out.println("z=" + z);
+	}
+	
+	
+	
+	public void method4() {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("문자열을 입력하세요 : "); 
+		sc.next();
+		
+		System.out.print("첫번째 문자 : ");
+		char c1 = '';
+		char c2 = '';
+		c1 = sc.nextLine.charAt(0);
+		char computer = line.charAt(0);
+		
+		// 이렇게 하는 게 맞나..? 쌤 제가 문제 의도를 제대로 읽은 게 맞나요... 막.. computer에 c위치 지정하고 그런 거 아니죠..? 
+	}
+	
+}
