e0f93ea680d6144c5c2ba20313f5ccd08c3a8848
ekato99 <133620407+cookiekingjlim@users.noreply.github.com>
2023-06-15 17:44:56 +0900

    feat

--- Twitter/src/com/twitter/controller/VideoController.java
+++ Twitter/src/com/twitter/controller/VideoController.java
@@ -1,22 +1,27 @@
 package com.twitter.controller;
 
+import java.util.ArrayList;
+
 import com.twitter.model.Video;
 
 public class VideoController {
 	
 	//트위터에 영상 업로드 할 때 리스트가 필요할까...?
-	VideoController vc = new VideoController();
+//	VideoController vc = new VideoController();
+	ArrayList<Video> videoList = new ArrayList<>();
 	
 	//영상 업로드
 	public void uploadVideo(Video video) {	
-		
+		videoList.add(video);
 	}
 	// 영상 수정..?
 	// 영상 한 개 보기
 	public Video viewVideo(int index) {
-		
+		return videoList.get(index);
 	}
 	
 	// 영상 삭제하기
-
+	public boolean deleteVideo(Video video) {
+		return videoList.remove(video);
+	}
 }
--- Twitter/src/com/twitter/model/Post.java
+++ Twitter/src/com/twitter/model/Post.java
@@ -1,5 +1,8 @@
 package com.twitter.model;
 
 public class Post {
+	
+	public User user; // User 정보 받기
+	
 
 }
